a76eafda52e85755cc8fd6d02726b5b691fce073
Aliaksandr Mazaleuski <Aliaksandr.Mazaleuski@britishgas.co.uk>
2023-06-24 22:31:44 +0200

    Fix critical bug

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm ci && npm run build && node dist/main.js && mkdir 1
+web: npm ci && npm run build && node dist/main.js
--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ import { AppModule } from './app.module';
 import { NodeProcesses } from './constants/process';
 import helmet from 'helmet';
 
-const path =
+const fullPath =
   process.env.NODE_ENV?.trim() === 'development'
-    ? path.resolve(__dirname, '.env')
+    ? '.env'
     : path.resolve('/', '/opt/elasticbeanstalk/deployment/env');
 
 require('dotenv').config({
-  path,
+  path: fullPath,
 });
 
 async function bootstrap() {
@@ -19,7 +19,7 @@ async function bootstrap() {
 
   app.use(helmet());
   console.log('--- Starting with ---');
-  console.log(path);
+  console.log(fullPath);
   console.log(process.env.NODE_ENV);
   console.log(process.env.RDS_DB_NAME);
   console.log(NodeProcesses());
