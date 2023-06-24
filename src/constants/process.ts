8de890ec37d0064ced735015683b27f94af7606a
Aliaksandr Mazaleuski <Aliaksandr.Mazaleuski@britishgas.co.uk>
2023-06-24 22:03:39 +0200

    Update dotenv for development

--- src/constants/process.ts
+++ src/constants/process.ts
@@ -1,10 +1,9 @@
-export const NodeProcesses = {
-  PORT: Number(process.env.PORT),
-
+export const NodeProcesses = () => ({
+  PORT: process.env.PORT,
   POSTGRES_DIALECT: process.env.POSTGRES_DIALECT,
-  POSTGRES_PORT: Number(process.env.POSTGRES_PORT),
+  POSTGRES_PORT: process.env.POSTGRES_PORT,
   POSTGRES_USER: process.env.POSTGRES_USER,
   POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
   POSTGRES_HOST: process.env.POSTGRES_HOST,
   POSTGRES_DB: process.env.POSTGRES_DB,
-};
+});
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,21 @@
-import * as path from 'path';
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NodeProcesses } from './constants/process';
 import helmet from 'helmet';
 
-require('dotenv').config(
-  process.env.NODE_ENV === 'development' && {
-    path: path.resolve(`.env.${process.env.NODE_ENV || 'development'}`).trim(),
-  },
-);
+if (process.env.NODE_ENV?.trim() === 'development') {
+  require('dotenv').config();
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
   console.log('--- Starting with ---');
-  console.log(JSON.stringify(NodeProcesses));
-  await app.listen(NodeProcesses.PORT || 3000);
+  console.log(process.env.NODE_ENV);
+  console.log(process.env.POSTGRES_DB);
+  console.log(NodeProcesses());
+  await app.listen(NodeProcesses().PORT || 8080);
 }
 
 bootstrap();
